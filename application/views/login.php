1bd33b5845318902688050b667835613cc9f582c
Unknown <danaralifian@gmail.com>
2017-11-22 22:07:04 +0700

    update view (22-11-2017)

---
+++ application/views/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/Google-Style-Login.css">
+</head>
+
+<body class="bg">
+    <div class="login-card"><img src="assets/img/avatar_2x.png" class="profile-img-card">
+        <p class="profile-name-card"> </p>
+        <form class="form-signin"><span class="reauth-email"> </span>
+            <input class="form-control" type="text" required="" placeholder="Nama Pengguna" autofocus="" id="inputEmail">
+            <input class="form-control" type="password" required="" placeholder="Kata Sandi" id="inputPassword">
+            <div class="checkbox">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox">Remember me</label>
+                </div>
+            </div>
+            <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit" onclick="">Sign in</button>
+        </form>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
