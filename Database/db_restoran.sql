603c0eb70331cf7c3c5f0493e8b93d67a8b9a4c5
Iqbal Ramadhani <iqbal.ramadhani@email.unikom.ac.id>
2017-11-23 21:04:59 +0700

    Update database
    Aku tambahin satu table lagi.
    Tabel pelanggan

--- Database/db_restoran.sql
+++ Database/db_restoran.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2017 at 03:06 AM
+-- Generation Time: Nov 23, 2017 at 03:02 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -45,6 +45,40 @@ INSERT INTO `makanan` (`id`, `nama`, `harga`, `foto`) VALUES
 (5, 'Pepes', 10000, ''),
 (6, 'Bakakak Hayam', 15000, '');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE IF NOT EXISTS `pelanggan` (
+`id_pelanggan` int(11) NOT NULL,
+  `nama` varchar(200) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelanggan`, `nama`, `tanggal`) VALUES
+(1, 'bale', '2017-11-23'),
+(2, 'coba', '2017-11-23'),
+(3, 'coba2', '2017-11-23'),
+(4, 'coba2', '2017-11-23'),
+(5, 'coba2', '2017-11-23'),
+(6, 'coba2', '2017-11-23'),
+(7, 'coba2', '2017-11-23'),
+(8, 'coba2', '2017-11-23'),
+(9, 'coba2', '2017-11-23'),
+(10, 'coba2', '2017-11-23'),
+(11, 'coba2', '2017-11-23'),
+(12, 'coba2', '2017-11-23'),
+(13, 'coba2', '2017-11-23'),
+(14, 'asd', '2017-11-23'),
+(15, 'asd', '2017-11-23'),
+(16, 'asd', '2017-11-23');
+
 --
 -- Indexes for dumped tables
 --
@@ -55,6 +89,21 @@ INSERT INTO `makanan` (`id`, `nama`, `harga`, `foto`) VALUES
 ALTER TABLE `makanan`
  ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+ ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
