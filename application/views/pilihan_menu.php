2772b4aa524aa79a59adf8070071324c1fc18ba1
Unknown <danaralifian@gmail.com>
2017-12-06 11:22:39 +0700

    edit-respon

--- application/views/pilihan_menu.php
+++ application/views/pilihan_menu.php
@@ -18,18 +18,18 @@
     <hr class="baris">
     <center>
     <div class="row">
-        <div class="col-sm-6 col-md-4">
+        <div class="col-sm-4 col-md-4">
             <a href="<?php echo base_url().'manager/';?>">
                 <input type="button" class="btn btn-default btn-circle btn-xl" value="Manager">
             </a>          
         </div>
-        <div class="col-sm-6 col-md-4">
+        <div class="col-sm-4 col-md-4">
             <a href="<?php echo base_url().'pelayan';?>">
                 <input type="button" class="btn btn-default btn-circle btn-xl" value="Pelayan">
             </a>
             <!--<button type="button" class="btn btn-default btn-circle btn-xl">Pelayan<i class=""></i></button>-->
         </div>
-        <div class="col-sm-6 col-md-4">
+        <div class="col-sm-4 col-md-4">
             <a href="<?php echo base_url().'koki';?>">
                 <input type="button" class="btn btn-default btn-circle btn-xl" value="Koki"></a>
             <!--<button type="button" class="btn btn-default btn-circle btn-xl">Koki</button>-->
@@ -38,16 +38,16 @@
     <div class="spasi"></div>
     
     <div class="row">
-        <div class="col-sm-6 col-md-4">
+        <div class="col-sm-4 col-md-4">
             <a href="<?php echo base_url().'pelanggan';?>">
                 <input type="button" class="btn btn-default btn-circle btn-xl" value="Pelanggan">
             </a>            
         </div>
-        <div class="col-sm-6 col-md-4">
+        <div class="col-sm-4 col-md-4">
             <a href="<?php echo base_url().'kasir';?>">
             <button type="button" class="btn btn-default btn-circle btn-xl">Kasir<i class=""></i></button>
         </div>
-        <div class="col-sm-6 col-md-4">
+        <div class="col-sm-4 col-md-4">
             <a href="<?php echo base_url().'pantry';?>">
                 <input type="button" class="btn btn-default btn-circle btn-xl" value="Pantry">
             </a>
